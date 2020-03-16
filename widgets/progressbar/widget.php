bed1404810f19a039080ea2e4cc219b7b2c5564a
Md Abu Talib <36687762+TALIB4590@users.noreply.github.com>
2020-03-16 13:46:24 +0600

    fixed misspelling

--- widgets/progressbar/widget.php
+++ widgets/progressbar/widget.php
@@ -129,7 +129,7 @@ class Progressbar_Widget extends Widget_Base {
 		$this->start_controls_section(
 			'_style_section_value',
 			array(
-				'label' => __( 'Title', 'dom' ),
+				'label' => __( 'Value', 'dom' ),
 				'tab'   => Controls_Manager::TAB_STYLE,
 			)
 		);
